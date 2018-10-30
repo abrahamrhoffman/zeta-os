2ec4b9133900ede6837f51adbc840eb96bee83ee
Abe Hoffman <abhoffman@paypal.com>
2018-10-30 11:25:52 -0700

    v0.2

---
+++ src/root/etc/filesystem.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir /mnt
+
+mount -t tmpfs none /mnt
+
+mkdir /mnt/dev
+mkdir /mnt/sys
+mkdir /mnt/proc
+mkdir /mnt/tmp
+
+mount --move /dev /mnt/dev
+mount --move /sys /mnt/sys
+mount --move /proc /mnt/proc
+#mount --move /tmp /mnt/tmp
+
+exec switch_root /mnt /etc/main_init.sh
---
+++ src/root/etc/main_init.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Busybox init. Based on configuration in /etc/inittab
+exec /sbin/init
