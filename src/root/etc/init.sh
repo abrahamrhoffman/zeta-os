1404ca2b52c083207630dc132eafdf3576aa9bfd
Abe Hoffman <abhoffman@paypal.com>
2018-11-01 00:35:02 -0700

    v0.2

--- src/root/etc/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-dmesg -n 1
-mount -t devtmpfs none /dev
-mount -t proc none /proc
-mount -t tmpfs none /tmp -o mode=1777
-mount -t sysfs none /sys
-mkdir -p /dev/pts
-mount -t devpts none /dev/pts
--- src/root/etc/main_init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Busybox init. Based on configuration in /etc/inittab
-#setsid cttyhack /bin/sh
-exec /sbin/init
